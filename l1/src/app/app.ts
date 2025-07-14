8413fa75c6844dee99072ccb7d3f47e726d88c0c
DhayanithiVasanthakumar <dhayanithivasanthakumar@gmail.com>
2025-07-14 09:57:22 +0530

    attribute directive

--- l1/src/app/app.ts
+++ l1/src/app/app.ts
@@ -4,10 +4,12 @@ import { DataBinding } from '../components/data-binding/data-binding';
 import { Admin } from '../components/admin/admin';
 import { NgIf } from "../components/ng-if/ng-if";
 import { NgFor } from '../components/ng-for/ng-for';
+import { NgClass } from '../components/ng-class/ng-class';
+import { NgStyle } from "../components/ng-style/ng-style";
 
 @Component({
   selector: 'app-root',
-  imports: [ NgIf,NgFor],
+  imports: [DataBinding, NgIf, NgFor, NgClass, NgStyle],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ l1/src/components/ng-style/ng-style.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-ng-style',
+  imports: [CommonModule,FormsModule],
+  templateUrl: './ng-style.html',
+  styleUrl: './ng-style.css'
+})
+export class NgStyle {
+
+  div1bgcolor:string="";
+  isChecked:boolean=false;
+
+  myCss:any={
+    'background-color': 'red',
+    'width': '200px',
+    'height': '200px',
+  }
+
+  addDiv1Color(color:string){
+    this.div1bgcolor=color;
+  }
+}
