2345a2da6a3b92805e0dd0dc5427519d42e41dc4
DhayanithiVasanthakumar <dhayanithivasanthakumar@gmail.com>
2025-07-22 09:03:02 +0530

    pipes

--- l1/src/components/ng-for/ng-for.ts
+++ l1/src/components/ng-for/ng-for.ts
@@ -1,23 +1,36 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { NaPipe } from '../../pipes/na-pipe';
 
 @Component({
   selector: 'app-ng-for',
-  imports: [CommonModule],
+  imports: [CommonModule,NaPipe],
   templateUrl: './ng-for.html',
   styleUrl: './ng-for.css'
 })
 export class NgFor {
+
+  //pipes
+  courseName:string="Angular";
+
+
+  studentObj:any={
+    name:"dhaya",
+    city:"namakkal",
+    mobile:"1122334455"
+  }
+
+  currentDate: Date=new Date;
 
   //array of list
   cityList:String[] = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
 
   //array of objects
   employeeList :any[] = [
-    {id: 1, name: "John Doe", position: "Software Engineer"},
+    {id: 1, name: "", position: "Software Engineer"},
     {id: 2, name: "Jane Smith", position: "Project Manager"},
-    {id: 3, name: "Sam Wilson", position: "UX Designer"},
+    {id: 3,  position: "UX Designer"},
     {id: 4, name: "Lisa Brown", position: "Data Analyst"},
-    {id: 5, name: "Tom White", position: "DevOps Engineer"}
+    {id: 5, name: null, position: "DevOps Engineer"}
   ]
 }
---
+++ l1/src/pipes/na-pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'na'
+})
+export class NaPipe implements PipeTransform {
+
+  transform(value: unknown): unknown {
+    if(value=='' || value==null || value==undefined){
+      return"NA";//NA=> not available
+    }else{
+       return value;
+    }
+
+   
+  }
+
+}
